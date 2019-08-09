9ad09ea1ed1683a6bd874fd5aa886448a15e2731
Mattx8y <wrscr.surpertop@gmail.com>
2019-08-08 23:36:09 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ TARGET_BRANCH="gh-pages"
 
 function docGen {
   npm run-script docs
-  mv ./out/node-bandwidth/*/* ./out
+  mv ./out/NodeNamelessMC/*/* ./out
 }
 NODE_VERSION=`node --version`
 NODE_VERSION=${NODE_VERSION:1:1}
@@ -50,15 +50,6 @@ fi
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
