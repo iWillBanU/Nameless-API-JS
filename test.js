e044688e897841004848d300ae0a98482e9dc8a7
Mattx8y <wrscr.surpertop@gmail.com>
2019-08-25 01:31:29 -0400

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-let Nameless = require(".");
-let Client = new Nameless.Client(process.env.API_URL);
-
-Client.register("Notch", "069a79f444e94726a5befca90e38aaf5", "test@exaple.com");
