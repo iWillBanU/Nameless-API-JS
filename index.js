d071adf0345209ae8f866b98a6f6fe7d3e549be5
Mattx8y <wrscr.surpertop@gmail.com>
2019-08-30 22:58:15 -0400

    Add User to module exports

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Client = require("./src/Client.js");
 const APIManager = require("./src/rest/APIManager.js");
+const User = require("./src/User.js");
 
-module.exports = {Client, APIManager};
+module.exports = {Client, APIManager, User};
