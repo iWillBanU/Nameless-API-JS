634a2ee4a381aacfbe616997a82b7b535d01b2bd
Mattx8y <wrscr.surpertop@gmail.com>
2019-09-02 15:00:02 -0400

    Create build.js

---
+++ build.js
@@ -0,0 +1,19 @@
+const pkg = require("./package.json");
+const fs = require("fs");
+const p = require("path");
+
+var deleteFolderRecursive = function(path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function(file, index){
+      var curPath = p.join(path,file);
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
+deleteFolderRecursive(`./${pkg.name}/${pkg.version}`);
