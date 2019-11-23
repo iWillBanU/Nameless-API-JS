584dc458e7e682b06712383426109ebe56b6f088
iWillBanU <wrscr.surpertop@gmail.com>
2019-11-23 19:36:24 +0000

    More work

--- src/Client.js
+++ src/Client.js
@@ -4,12 +4,14 @@ const APIManager = require("./rest/APIManager.js");
 
 /**
  * The main client class
+ * @class
  */
 class Client {
   /**
    * Creates a new client class
    * @param {String} apiUrl The API url
    * @param {Boolean} cacheUsers Whether or not to cache user objects. (improves preformance but may cause inconsistentcies between the library and the website) Defaults to true
+   * @constructor
    */
   constructor(apiUrl, cacheUsers = true) {
     /**
@@ -21,6 +23,7 @@ class Client {
     /**
      * Whether or not to cache user objects
      * @type {Boolean}
+     * @property
      */
     this.cacheUsers = cacheUsers;
     
@@ -29,6 +32,7 @@ class Client {
        * A Map containing the cached users
        * @type {?Map<Array<String>,User>}
        * @private
+       * @property
        */
       this.cachedUsers = new Map();
     };
@@ -36,6 +40,7 @@ class Client {
     /**
      * The parsed URL object for the API url
      * @type {URL}
+     * @property
      */
     this.urlInfo = new url.URL(this.apiUrl);
 
@@ -43,6 +48,7 @@ class Client {
      * The API Manager for the client
      * @type {APIManager}
      * @private
+     * @property
      */
     this.apiManager = new APIManager(this);
 
@@ -53,12 +59,13 @@ class Client {
    * @param {String} uuid The Minecraft UUID of the new user
    * @param {String} email The email address of the new user
    * @returns {Promise}
+   * @method
    */
   register(username, uuid, email) {
     return new Promise(function(resolve, reject) {
       this.apiManager.executeAction("register", {username: username, uuid: uuid, email: email})
         .then(function(response) {
-          console.log(response);
+          // TODO: Handle response
           resolve();
         })
         .catch(reject);
@@ -69,6 +76,7 @@ class Client {
    * @param {String} user Minecraft username/UUID of the user to fetch
    * @param {Boolean} force? Force a request to the website/disables getting user from cache
    * @returns {Promise<User>} The user that was fetched
+   * @method
    */
   get(user, force = false) {
     // TODO: Cache Support
@@ -85,6 +93,27 @@ class Client {
         }).catch(reject);
     });
   };
+  /**
+   * Sets a user's group on the website
+   * @param {String} user Minecraft username/UUID of the user whos group is being set
+   * @param {Number} group The ID of the group that is being changed
+   */
+  setGroup(user, group) {
+    let postData = {group_id: group};
+    if (user.length <= 15) {
+        postData.username = user;
+    } else {
+        postData.uuid = user;
+    };
+    return new Promise(function(resolve, reject) {
+        this.apiManager.executeAction("setGroup", postData)
+            .then(function(response) {
+                // TODO: Handle response
+                resolve();
+            }).catch(reject);
+    });
+  };
+  
 };
 
 module.exports = Client;
